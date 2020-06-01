0271416155f5a245537789370258e589b534c6a5
Erick <yaeriks@yandex.ru>
2020-06-01 12:52:56 +0300

    Add files via upload

---
+++ cfg/languages/en.lng.php
@@ -0,0 +1,147 @@
+<?php
+
+$ntodayz = 'Today total played: ';
+
+$nocommand = 'You use unregistered command, use';
+$stats_top = 'Top';
+$stats_skill = 'Skill';
+$day_top = 'DAY TOP';
+$played_top = 'Played';
+$week_top = 'WEEK TOP';
+ 
+
+//ftp_alloc
+$sentft = 'The link to the FTP server $ftp_server';
+$senterror = 'Unable to establish a connection with the FTP server! Trying to connect to the server $ftp_server!';
+$sentokkk = 'The files have been sent!';
+
+/// Delete
+$admdbdeleted = 'Admin database was deleted!';
+$topdbdeleted = 'Player stats database deleted!';
+$bandbdeleted = 'Player ban database deleted!';
+$bandbnoob = 'Admin you are noob? Use number of player place from !top';
+$ttdbdeleted = 'Player Nr';
+$tttdbdltd = 'was delete from Top!';
+
+/// refresh
+$rfnonee = 'None';
+$rfnonply = 'No one in player top list!';
+$rfshh = 'Refresh top.';
+$rfshb = 'Refresh banlist.';
+
+/// screen
+$getssj = 'Screenshot was sent to server!';
+$getssx = 'Players Screenshots was sent to server!';
+$rfsh = 'Admin database was deleted!';
+
+//login
+$loginnx = 'You already in RCM system, dont need do that again!';
+$loggran = 'Crangulations';
+$logginn = 'you in';
+$loggithx = 'group, ^3thanks for login!';
+$loggistop = 'You already in RCM system, dont need do that again!';
+$loggistopk = 'You already registered in RCM system!';
+$loggistopkk = 'you in ^1GUID ADMIN ^3group, ^3thanks for login!';
+$loggmemb = 'you in ^2Member group, ^3thanks for registering!';
+$loggplayer = 'you in ^2Player group, ^3thanks for quit!';
+
+//info
+$xwyears = "years";
+$xwmonths = "months";
+$xwdays = "days";
+$xwhours = "hours";
+$swminutes = "minutes";
+$swseconds = "seconds";
+
+$infoowrnn = 'WARNING';
+$infoowrnx = 'its not your group commands!';
+$infoowrnw = 'its only for admins!';
+$infoservv = 'Server';
+$infolx = 'Iam from';
+$infooip = 'IP';
+$infoocity = 'City';
+$infoocountry = 'Country';
+$infoodate = 'Date';
+$infootime = 'Time';
+$inforsun = 'Sunrise';
+$sunnsett = 'Sunset';
+$infooby = 'By';
+$infoonick = 'Nick';
+$infotodaytop = 'Today top';
+$playersinfo = 'Players';
+$playersbeest = 'Best player';
+$playersbased = 'Total visited';
+$bannedtotally = 'Banned';
+$infooguid = 'Guid';
+$infooreas = 'Reason';
+$infoonggame = 'Game';
+$infoofrom = 'From';
+$infoostat = 'Status';
+$infootop = 'Top';
+$infoorank = 'Rank';
+$infoosuic = 'Suicides';
+$infoobash = 'Bash';
+$infoolvvl = 'Level';
+$infoofrag = 'Frags';
+$infoohddd = 'HeadShots';
+$infoodth = 'Deaths';
+$infoortio = 'Ratio';
+$infoosklll = 'Skill';
+$infoorrnk = 'Skill Rank';
+$infooplydx = 'Played';
+$infoogrnkll = 'Grenade Kills';
+$infoohnt = 'Last Hunt';
+$infoosttta = 'Negative stats!';
+$infoorngg = 'IP Range is not correct! (EXAMPLE: !range 230.168.1.0-230.168.1.100 OR !range 230.168.*.*  OR !range 230.168.0.0/16 )';
+$infoomapz = 'Admin changed map to';
+$infoorell = 'Admin reloaded map';
+$infoogtxx = 'Admin changed gametype to';
+$infobanx = 'BAN By Admin';
+$infoflss = '^1False Map! ^2Only:';
+$infoflsg = '^1False gametype!';
+$infoompls = '^1Maplist:';
+$infoomnxtt = '^1Nextmap:';
+$fllddf = 'Flooding';
+$tmpbnd = 'TEMPBAN';
+$noospmm = 'NO Spam';
+$noohldd = 'You are no one holds here!';
+$cnsorrd = 'Censored!';
+$cnsorrdsmz = 'stop swearing or you get a kick';
+$cnsorrdsmzjy = 'you get a ban';
+$sysupttm = 'System uptime';
+$sysrkgd = 'System';
+$sysrkcpuu = 'CPU';
+
+///ban
+$proxyxn = 'Proxy\VPN Blacklisted IP adress ^1Detected! by';
+$flldd = 'Stop chat flooding or BAN';
+
+//stats
+$stsnoskl = 'No Skill Rank. Nope experience...';
+$stsnoextt = 'Do not exist!';
+
+///FUN
+$thxq = 'Thank you too';
+$gggq = 'Good Game';
+$fckqq1 = '^7night 1 ^3(_._), ^7night 2 ^3(_o_), ^7night 3 ^3(_0_), ^7night 4 ^3(_((0))_)';
+$fckqq2 = '^7u dirty boy!';
+$vodqqq = '^7\_/ \_/ \_/ ^4VODKA CONNECTING PEOPLE';
+$merrycrr = 'Merry Christmas ^1*^7<(^5:^7{^1D';
+$nyyycrr = 'Happy New Year ^1*^7<(^5:^7{^1O';
+$afffk = 'Away from keyboard!';
+
+$pppanix = 'Do not panic, sent ^7'.$ixz.'report ^1with your problem message!';
+$adminppp = 'Sent ^7'.$ixz.'support ^1with your question & in message add your e-mail!';
+$reppport = 'Message has been sent to Admin!';
+
+//another
+$ban_name = "^3You Banned by Admins ^2".$website."";
+$ban_name_all = "^1Banned ";
+$ban_ip = "^3You Banned by Admins ^2".$website."";
+$ban_ip_all = "^1Banned ";
+$c_unban = "^2UnBanned ";
+$rules_bad_name_msg = "^6||| ^3Rename - you're breaking the rules";
+$rules_msgtoall_kicked_bad_name = "Player %s has bug name.";
+$welcome_x = "Welcome";   
+$welcome_e = "Bye";
+$welcome_x2 = "Welcome Back";	
---
+++ cfg/languages/ru.lng.php
@@ -0,0 +1,146 @@
+<?php
+
+$ntodayz = 'Сегодня играло игроков: ';
+
+$nocommand = 'Вы используете незарегистрированную команду, используйте';
+$stats_top = 'Топ';
+$stats_skill = 'Навык(а)';
+$day_top = 'Топ дня';
+$played_top = 'играет';
+$week_top = 'Топ недели';
+ 
+//ftp_alloc
+$sentft = 'Установлено соединение с FTP сервером $ftp_server';
+$senterror = 'Не удалось установить соединение с FTP сервером! Попытка подключения к серверу $ftp_server!';
+$sentokkk = 'Файлы были отправлены!';
+
+/// Delete
+$admdbdeleted = 'База данных Администраторов удаленa!';
+$topdbdeleted = 'База данных Статистики игроков удаленa!';
+$bandbdeleted = 'База данных забаненых игроков удаленa!';
+$bandbnoob = 'Админ: Вы нуб? Используйте номер игрока c места в топe';
+$ttdbdeleted = 'Игрок Nr';
+$tttdbdltd = 'был удален с топа!';
+
+/// screen
+$getssj = 'Скриншот был отправлен на сервер!';
+$getssx = 'Скриншоты игрокoв были отправлены на сервер!';
+$rfsh = 'База данных Администраторов былa удаленa!';
+
+/// refresh
+$rfnonee = 'Нeтy';
+$rfnonply = 'Никого в топ списке игроков!';
+$rfshh = 'Oбновление топ.';
+$rfshb = 'Oбновление забаненых.';
+
+//login
+$loginnx = 'Вы уже в системе RCM, не нужно делать это снова!';
+$loggran = 'Поздровляем';
+$logginn = 'ты в';
+$loggithx = 'группе, ^3спасибо за логин!';
+$loggistop = 'Вы уже в системе RCM, не нужно делать это снова!';
+$loggistopk = 'Вы уже зарегистрированы в системе RCM!';
+$loggistopkk = 'вы в ^1GUID ADMIN ^3группе, ^3спасибо за логин!';
+$loggmemb = 'вы в ^2Member группе, ^3спасибо за регистрацию!';
+$loggplayer = 'вы в ^2Player группе, ^3спасибо за выход!';
+
+//info
+$xwyears = "лет";
+$xwmonths = "месяцев";
+$xwdays = "дней";
+$xwhours = "часов";
+$swminutes = "минут";
+$swseconds = "секунд";
+
+$infoowrnn = 'Предупреждение';
+$infoowrnx = 'не ваша группа команд!';
+$infoowrnw = 'только для админов!';
+$infoservv = 'Сервер';
+$infolx = 'Я из';
+$infooip = 'IP';
+$infoocity = 'Город';
+$infoocountry = 'Страна';
+$infoodate = 'Дата';
+$infootime = 'Время';
+$inforsun = 'Восход';
+$sunnsett = 'Закат';
+$infooby = 'By';
+$infoonick = 'Игрок';
+$infotodaytop = 'Сегодня в топе';
+$playersinfo = 'Игроков';
+$playersbeest = 'Лучший игрок';
+$playersbased = 'Всего посетило';
+$bannedtotally = 'Забанено';
+$infooguid = 'GUID';
+$infooreas = 'Причина';
+$infoonggame = 'Игрa';
+$infoofrom = 'из';
+$infoostat = 'Статус';
+$infootop = 'Топ';
+$infoorank = 'Ранг';
+$infoosuic = 'Самоубийств';
+$infoobash = 'Ножом';
+$infoolvvl = 'Уровень';
+$infoofrag = 'Фраги';
+$infoohddd = 'Хедшот';
+$infoodth = 'Смертей';
+$infoortio = 'k/d';
+$infoosklll = 'Навык';
+$infoorrnk = 'Ранг Навыкa';
+$infooplydx = 'Играл';
+$infoogrnkll = 'гранатой';
+$infoohnt = 'Играл';
+$infoosttta = 'Негативная статистика!';
+$infoorngg = 'IP Выбор не правильный! (Пример !range 230.168.1.0-230.168.1.100 игрок или !range 230.168.*.*  или !range 230.168.0.0/16 )';
+$infoomapz = 'Админ сменил карту на';
+$infoorell = 'Админ перезагрузил карту';
+$infoogtxx = 'Админ сменил Тип Игры на';
+$infobanx = 'Запрет администраторoм';
+$infoflss = '^1Ложнaя Картa! ^2Только:';
+$infoflsg = '^1Ложный Тип Игры!';
+$infoompls = '^1Список карт:';
+$infoomnxtt = '^1Следующия карта:';
+$fllddf = 'Флуд';
+$tmpbnd = 'Временный бан';
+$noospmm = 'Никакого спама';
+$noohldd = 'Вас никто не держит здесь!';
+$cnsorrd = 'Цензура!';
+$cnsorrdsmz = 'перестань ругаться или тебя пнут от сюда';
+$cnsorrdsmzjy = 'получает бан';
+$sysupttm = 'Работа системы';
+$sysrkgd = 'Система';
+$sysrkcpuu = 'Процессор';
+
+///ban
+$proxyxn = 'Прокси\ВПН черный список IP-адреса ^1Обнаружено! по';
+$flldd = 'Перестань флудить или бан';
+
+//stats
+$stsnoskl = 'Никакого Навыка. Опыта нету...';
+$stsnoextt = 'Отсутствует!';
+
+///FUN
+$thxq = 'Спасибо';
+$gggq = 'Хорошая Игра';
+$fckqq1 = '^7ночь 1 ^3(_._), ^7ночь 2 ^3(_o_), ^7ночь 3 ^3(_0_), ^7ночь 4 ^3(_((0))_)';
+$fckqq2 = '^7ты грязный мальчик!';
+$vodqqq = '^7\_/ \_/ \_/ ^4ВОДКА ОБЬЕДИНЯЕТ ЛЮДЕЙ';
+$merrycrr = 'С Рождеством ^1*^7<(^5:^7{^1D';
+$nyyycrr = 'Счастливого Нового Года ^1*^7<(^5:^7{^1O';
+$afffk = 'Вдали от клавиатуры!';
+
+$pppanix = 'Не паникуйте, пошлите ^7'.$ixz.'report ^1с проблемным сообщением!';
+$adminppp = 'Пошлите ^7'.$ixz.'support ^1с вопросом!';
+$reppport = 'Сообщение было отправлено администратору!';
+
+//another
+$ban_name = "^3Тебя забанили Администраторы ^2".$website."";
+$ban_name_all = "^1Забанен ";
+$ban_ip = "^Тебя забанили Администраторы ^2".$website."";
+$ban_ip_all = "^1Забанен ";
+$c_unban = "^2Разбанен ";
+$rules_bad_name_msg = "^6||| ^3Rename - you're breaking the rules";
+$rules_msgtoall_kicked_bad_name = "Player %s has bug name.";
+$welcome_x = "Добро пожаловать"; 
+$welcome_e = "До Свидания"; 
+$welcome_x2 = "Рады видеть снова";	
