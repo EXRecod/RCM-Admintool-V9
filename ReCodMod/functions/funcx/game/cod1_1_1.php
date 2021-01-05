a7eed2182f702486dff2b24a82b7fe5327b2a92d
Erick <yaeriks@yandex.ru>
2021-01-05 14:51:41 +0200

    Add files via upload

--- ReCodMod/functions/funcx/game/cod1_1_1.php
+++ ReCodMod/functions/funcx/game/cod1_1_1.php
@@ -16,7 +16,7 @@ if (strpos($parseline, ' K;') !== false) {
                                 if (($oguid != fakeguid(trim($killer_player_name))) && ($oid == $idkill)) {
                                     $go = 1;
                                     if ((rand(5, 15) < 7)) 
-										xcon('say ' . $i_name . ' ^7(' . $killer_player_name . ') ^1Forbidden to change nicknames or so many symbols (^)!.', '');
+										rcon('say ' . $i_name . ' ^7(' . $killer_player_name . ') ^1Forbidden to change nicknames or so many symbols (^)!.', '');
                                     //usleep(980000);
                                     //xcon('clientkick ' . $idk, '');
                                     
@@ -48,13 +48,16 @@ if (strpos($parseline, ' K;') !== false) {
 				if (empty($stats_error[$plarid]['no_nickname_change_timer']))
 				          $stats_error[$plarid]['no_nickname_change_timer'] = time();	  
 						  
-			          if(time() - $stats_error[$plarid]['no_nickname_change_timer'] > 10)
+			          if(time() - $stats_error[$plarid]['no_nickname_change_timer'] > 50)
 						  {
 							$stats_error[$plarid]['no_nickname_change_timer'] = time(); 
 							usleep(20000);
-							xcon('say ^3FAKE => ^1['.$killer_player_name.']' . ' ^7Your nick: ^7'.$stats_error[$plarid]['no_nickname_change'].' ^1STATS BLOCKED!', '');	
-							$killer_player_name = 'nickname_faker'; 
-						  }		
+							rcon('say ^3FAKE => ^1['.$killer_player_name.'] ^7Your nick: ^7'.$stats_error[$plarid]['no_nickname_change'].' ^1STATS BLOCKED!', '');	
+						  $killer_player_name = 'nickname_faker';
+						  //$death_player_name = 'nickname_faker';
+						  }
+						  $killer_player_name = 'nickname_faker';
+						  //$death_player_name = 'nickname_faker';
 					  } 		
 				}
 				
@@ -73,15 +76,50 @@ if (strpos($parseline, ' K;') !== false) {
 
 					if(($stats_error[$plarid]['name']) != trim($death_player_name))
 					  {
-			          if(time() - $stats_error[$plarid]['no_nickname_change_timer'] > 10)
+			          if(time() - $stats_error[$plarid]['no_nickname_change_timer'] > 50)
 						  {
 							$stats_error[$plarid]['no_nickname_change_timer'] = time(); 
 							usleep(20000);
-							xcon('say ^3FAKE => ^1['.$death_player_name.']' . ' ^7Your nick: ^7'.$stats_error[$plarid]['no_nickname_change'].' ^1STATS BLOCKED!', '');	
-							$death_player_name = 'nickname_faker'; 
+							rcon('say ^3FAKE => ^1['.$death_player_name.']' . ' ^7Your nick: ^7'.$stats_error[$plarid]['no_nickname_change'].' ^1STATS BLOCKED!', '');	
+						  //$killer_player_name = 'nickname_faker';
+						  $death_player_name = 'nickname_faker'; 
 						  }		
-					  } 		
-				}				
+					  	  //$killer_player_name = 'nickname_faker';
+						  $death_player_name = 'nickname_faker';
+					  } 
+				}
+
+	if(!empty($connect_error[$idnumb]['nickname_change']))
+	{
+						  //$killer_player_name = 'nickname_faker';
+						  $death_player_name = 'nickname_faker'; 
+						  
+					  if(!empty($connect_error[$idnumb]['nickname_timers']))
+	                      {
+			          if(time() - $connect_error[$idnumb]['nickname_timers'] > 50)
+						  {
+							$connect_error[$idnumb]['nickname_timers'] = time(); 
+							usleep(20000);
+							rcon('say ^3CHANGE NICKNAME: ^7'.$connect_error[$idnumb]['nickname_change'].' ^3AND RECONNECT ^1STATS BLOCKED!(NICKNAME USED)', '');	
+						  }							
+						  }
+	}
+	
+ 	if(!empty($connect_error[$idkill]['nickname_change'])) 
+	{
+						  $killer_player_name = 'nickname_faker';
+						  //$death_player_name = 'nickname_faker'; 
+					  if(!empty($connect_error[$idkill]['nickname_timers']))
+	                      {
+			          if(time() - $connect_error[$idkill]['nickname_timers'] > 50)
+						  {
+							$connect_error[$idkill]['nickname_timers'] = time(); 
+							usleep(20000);
+							rcon('say ^3CHANGE NICKNAME: ^7'.$connect_error[$idkill]['nickname_change'].' ^3AND RECONNECT ^1STATS BLOCKED!(NICKNAME USED)', '');	
+						  }							
+						  }
+						  
+	}		
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////				
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////				
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -96,6 +134,7 @@ if (strpos($parseline, ' K;') !== false) {
 else if (strpos($parseline, ' D;') !== false) {
 	$rconstatus = 1;
 	
+    list($num, $idnumb, $vv4, $death_player_name, $idkill, $vv8, $killer_player_name, $byweapon, $vv11, $modkll, $hitlock) = explode(';', $parseline);	
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -115,11 +154,11 @@ else if (strpos($parseline, ' D;') !== false) {
 				if (empty($stats_error[$plarid]['no_nickname_change_timer']))
 				          $stats_error[$plarid]['no_nickname_change_timer'] = time();	  
 						  
-			          if(time() - $stats_error[$plarid]['no_nickname_change_timer'] > 10)
+			          if(time() - $stats_error[$plarid]['no_nickname_change_timer'] > 50)
 						  {
 							$stats_error[$plarid]['no_nickname_change_timer'] = time(); 
 							usleep(20000);
-							xcon('say ^3FAKE => ^1['.$killer_player_name.']' . ' ^7Your nick: ^7'.$stats_error[$plarid]['no_nickname_change'].' ^1STATS BLOCKED!', '');	
+							rcon('say ^3FAKE => ^1['.$killer_player_name.'] ^7Your nick: ^7'.$stats_error[$plarid]['no_nickname_change'].' ^1STATS BLOCKED!', '');	
 							$killer_player_name = 'nickname_faker'; 
 						  }		
 					  } 		
@@ -140,21 +179,52 @@ else if (strpos($parseline, ' D;') !== false) {
 
 					if(($stats_error[$plarid]['name']) != trim($death_player_name))
 					  {
-			          if(time() - $stats_error[$plarid]['no_nickname_change_timer'] > 10)
+			          if(time() - $stats_error[$plarid]['no_nickname_change_timer'] > 50)
 						  {
 							$stats_error[$plarid]['no_nickname_change_timer'] = time(); 
 							usleep(20000);
-							xcon('say ^3FAKE => ^1['.$death_player_name.']' . ' ^7Your nick: ^7'.$stats_error[$plarid]['no_nickname_change'].' ^1STATS BLOCKED!', '');	
+							rcon('say ^3FAKE => ^1['.$death_player_name.'] ^7Your nick: ^7'.$stats_error[$plarid]['no_nickname_change'].' ^1STATS BLOCKED!', '');	
 							$death_player_name = 'nickname_faker'; 
 						  }		
 					  } 		
-				}				
+				}
+
+
+	if(!empty($connect_error[$idnumb]['nickname_change']))
+	{
+						  $killer_player_name = 'nickname_faker';
+						  $death_player_name = 'nickname_faker'; 
+						  
+					  if(!empty($connect_error[$idnumb]['nickname_timers']))
+	                      {
+			          if(time() - $connect_error[$idnumb]['nickname_timers'] > 50)
+						  {
+							$connect_error[$idnumb]['nickname_timers'] = time(); 
+							usleep(20000);
+							rcon('say ^3CHANGE NICKNAME: ^7'.$connect_error[$idnumb]['nickname_change'].' ^3AND RECONNECT ^1STATS BLOCKED!(NICKNAME USED)', '');	
+						  }							
+						  }
+	}
+	
+ 	if(!empty($connect_error[$idkill]['nickname_change'])) 
+	{
+						  $killer_player_name = 'nickname_faker';
+						  $death_player_name = 'nickname_faker'; 
+					  if(!empty($connect_error[$idkill]['nickname_timers']))
+	                      {
+			          if(time() - $connect_error[$idkill]['nickname_timers'] > 50)
+						  {
+							$connect_error[$idkill]['nickname_timers'] = time(); 
+							usleep(20000);
+							rcon('say ^3CHANGE NICKNAME: ^7'.$connect_error[$idkill]['nickname_change'].' ^3AND RECONNECT ^1STATS BLOCKED!(NICKNAME USED)', '');	
+						  }							
+						  }
+						  
+	}				
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////				
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////				
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////	
 	
-	
-    list($num, $idnumb, $vv4, $death_player_name, $idkill, $vv8, $killer_player_name, $byweapon, $vv11, $modkll, $hitlock) = explode(';', $parseline);
     $parseline = $num . ';' . fakeguid(trim($death_player_name)) . ';' . $idnumb . ';' . $vv4 . ';' . $death_player_name . ';' . fakeguid(trim($killer_player_name)) . ';' . $idkill . ';' . $vv8 . ';' . $killer_player_name . ';' . $byweapon . ';' . $vv11 . ';' . $modkll . ';' . $hitlock;
 }
 else if (strpos($parseline, ' J;') !== false) {
@@ -163,6 +233,7 @@ else if (strpos($parseline, ' J;') !== false) {
     $parseline = $num . ';' . fakeguid(trim($player_name)) . ';' . $nm . ';' . $player_name;
 
 	if(!empty($stats_error[$nm])){ unset($stats_error[$nm]); echo "\n CLEAR $num ";}
+    if(!empty($connect_error[$nm])){ unset($connect_error[$nm]); echo "\n CLEAR $num ";}	
 
 }
 else if (strpos($parseline, ' Q;') !== false) {
@@ -173,7 +244,7 @@ else if (strpos($parseline, ' Q;') !== false) {
     unset($player_geoip_ctrl['' . $nm . '']);
 	
 	if(!empty($stats_error[$nm])){ unset($stats_error[$nm]); echo "\n CLEAR $num ";}
-
+    if(!empty($connect_error[$nm])){ unset($connect_error[$nm]); echo "\n CLEAR $num ";}
 	
 }
 else if ((strpos($parseline, ' say;') !== false) || (strpos($parseline, ' sayteam;') !== false)) {
@@ -214,7 +285,7 @@ if($stats_array[$conisq]['ip_adress'] == ''.$row['s_adm'].'')
                 $plarid = $i_id;
 				if (empty($stats_error[$plarid]['name']))
 				{					
-					$stats_error[$plarid]['name'] = trim($death_player_name);
+					$stats_error[$plarid]['name'] = trim($i_name);
 				}
                 else if (!empty($stats_error[$plarid]['name']))
 				{
@@ -224,16 +295,18 @@ if($stats_array[$conisq]['ip_adress'] == ''.$row['s_adm'].'')
 				if (empty($stats_error[$plarid]['no_nickname_change_timer']))
 				          $stats_error[$plarid]['no_nickname_change_timer'] = time();					
 
-					if(($stats_error[$plarid]['name']) != trim($death_player_name))
+					if(($stats_error[$plarid]['name']) != trim($i_name))
 					  {
-			          if(time() - $stats_error[$plarid]['no_nickname_change_timer'] > 10)
+			          if(time() - $stats_error[$plarid]['no_nickname_change_timer'] > 50)
 						  {
 							$stats_error[$plarid]['no_nickname_change_timer'] = time(); 
 							usleep(20000);
-							xcon('say ^3FAKE => ^1['.$player_name.']' . ' ^7Your nick: ^7'.$stats_error[$plarid]['no_nickname_change'].' ^1CHAT BLOCKED!', '');	
+							rcon('say ^3FAKE => ^1['.$player_name.'] ^7Your nick: ^7'.$stats_error[$plarid]['no_nickname_change'].' ^1CHAT BLOCKED!', '');	
 							$player_name       = $stats_error[$plarid]['no_nickname_change']; 
 							$react             = '^1disabled fake chat';
-						  }		
+						  }
+                            $player_name       = $stats_error[$plarid]['no_nickname_change']; 
+							$react             = '^1disabled fake chat';						  
 					  } 		
 				}				
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////				
